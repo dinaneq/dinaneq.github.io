1b32e04d3a600a5c58ab4f2484978ce19672ae07
dinaneq <dinanda@fitrianto.biz.id>
2024-10-03 00:11:08 +0700

    Update arsip.py

--- arsip.py
+++ arsip.py
@@ -35,20 +35,21 @@ def main():
         
     
     )
-    # From 2 to 10 items in one media group
-    # https://core.telegram.org/bots/api#sendmediagroup
-    media_group = list()
     for f in file_paths:
         with open(f, "rb") as fin:
-            # Up to 1024 characters.
-            # https://core.telegram.org/bots/api#inputmediadocument
-            caption = f"Daily Update for subapi: https://api.vmess.free.nf/{f} \n\n Total Accounts: {len(fin.readlines())}\n Updated on: {current_time}"
+            count = len(fin.readlines())
             # After the len(fin.readlines()) file's current position
             # will be at the end of the file. seek(0) sets the position
             # to the begining of the file so we can read it again during
             # sending.
             fin.seek(0)
-            media_group.append(InputMediaDocument(fin, caption=caption))
+            bot.send_document(
+                CHAT_ID,
+                document=fin,
+                # Up to 1024 characters.
+                # https://core.telegram.org/bots/api#inputmediadocument
+                caption = f"Daily Update for subapi: https://api.vmess.free.nf/{f} \n\n Total Accounts: {len(fin.readlines())}\n Updated on: {current_time}"
+            )
 
     bot.send_media_group(CHAT_ID, media=media_group)
 
