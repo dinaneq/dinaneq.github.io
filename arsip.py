8fc90bfc916c7cbbd506e697ed7f61588fa11b10
dinaneq <dinanda@fitrianto.biz.id>
2024-10-03 00:00:09 +0700

    Create arsip.py

---
+++ arsip.py
@@ -0,0 +1,57 @@
+from telegram import Bot, InputMediaDocument
+import datetime
+import pytz
+
+BOT_TOKEN = "7260168327:AAEEOvNziECXLf29xfIU9rsr9JqIcmOFR_g"
+CHAT_ID = -1001311543470
+current_time = datetime.datetime.now(pytz.timezone('Asia/Jakarta'))
+
+def main():
+    bot = Bot(BOT_TOKEN)
+    file_paths = (
+        "sub-all",
+        "aktif.txt",
+        "semua.txt",
+        "kumpulan-p1.txt",
+        "kumpulan-p2.txt",
+        "kumpulan-p3.txt",
+        "subapi",
+        "v2tel_link.txt",
+        "v2tel_links1.txt",
+        "v2tel_links2.txt",
+        "cm1.yml",
+        "cm2.yml",
+        "cm3.yml",
+        "tgproxy.txt",
+        "merge24.txt",
+        "singbox.json",
+        "nekobox.txt",
+        "sub-all",
+        "sub-reality",
+        "sub-ss",
+        "sub-trojan",
+        "sub-vless",
+        "sub-vmess",
+        
+    
+    )
+    # From 2 to 10 items in one media group
+    # https://core.telegram.org/bots/api#sendmediagroup
+    media_group = list()
+    for f in file_paths:
+        with open(f, "rb") as fin:
+            # Up to 1024 characters.
+            # https://core.telegram.org/bots/api#inputmediadocument
+            caption = f"Daily Update for subapi: https://api.vmess.free.nf/{f} \n\n Total Accounts: {len(fin.readlines())}\n Updated on: {current_time}"
+            # After the len(fin.readlines()) file's current position
+            # will be at the end of the file. seek(0) sets the position
+            # to the begining of the file so we can read it again during
+            # sending.
+            fin.seek(0)
+            media_group.append(InputMediaDocument(fin, caption=caption))
+
+    bot.send_media_group(CHAT_ID, media=media_group)
+
+
+if __name__ == "__main__":
+    main()
