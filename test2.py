18bb9d8be8b77d6d37e56377e8b0360d0d4485c0
dinaneq <dinanda@fitrianto.biz.id>
2024-10-02 21:29:59 +0700

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,14 @@
+import telegram
+
+def send_msg(text):
+    token = "7260168327:AAEEOvNziECXLf29xfIU9rsr9JqIcmOFR_g"
+    chat_id = "-1002249084770"
+    bot = telegram.Bot(token=token)
+    for i in text:
+        bot.sendMessage(chat_id=chat_id, text=i)
+
+new_list = []
+with open("sub-vless", 'r', encoding="utf-8") as file:
+     new_list = file.read()
+for i in new_list:
+     send_msg(i)
