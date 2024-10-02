d2a92a2045e26aeec8171016adf3fc32df21940d
dinaneq <dinanda@fitrianto.biz.id>
2024-10-02 18:29:28 +0700

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ async def send_document(document, chat_id):
 async def main():
   
     # Sending a document
-    await send_document(document=open('/sub-all', 'rb'), chat_id=chat_id)
+    await send_document(document=open('sub-all', 'rb'), chat_id=chat_id)
 
 if __name__ == '__main__':
     asyncio.run(main())
