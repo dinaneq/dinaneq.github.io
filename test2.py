e797b6af0b10fe0a49ead1f8267ed36668a46b95
dinaneq <dinanda@fitrianto.biz.id>
2024-10-02 21:34:00 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,7 +2,7 @@ import telegram
 
 def send_msg(text):
     token = "7260168327:AAEEOvNziECXLf29xfIU9rsr9JqIcmOFR_g"
-    chat_id = "-1002249084770"
+    chat_id = "5161912359"
     bot = telegram.Bot(token=token)
     for i in text:
         bot.sendMessage(chat_id=chat_id, text=i)
