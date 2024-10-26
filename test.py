d67a773bd1b9d7c48ce28e9208e4e0cb32c405a9
dinaneq <dinanda@fitrianto.biz.id>
2024-10-26 20:07:57 +0700

    Update test.py

--- test.py
+++ test.py
@@ -9,12 +9,9 @@ current_time = datetime.datetime.now(pytz.timezone('Asia/Jakarta'))
 def main():
     bot = Bot(BOT_TOKEN)
     file_paths = (
-        "sub-all",
-        "sub-reality",
-        "sub-ss",
-        "sub-trojan",
-        "sub-vless",
-        "sub-vmess",
+        "semua.txt",
+        "aktif.txt",    
+        "vmess",    
     
     )
     # From 2 to 10 items in one media group
